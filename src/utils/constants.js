018dbc68068675646bcd5eb810d09576bb3d9851
Brayan Delgado <bdelgadollalli@gmail.com>
2025-06-19 00:49:18 -0500

    Fix: usar backend en producción desde constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-export const BASE_API = 'http://127.0.0.1:8000/api';
+export const BASE_API = 'https://llaytech-restaurante.onrender.com';
 export const TOKEN = 'token';
 
 export const ORDER_STATUS = {
